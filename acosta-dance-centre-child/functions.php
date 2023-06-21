a5c5d5cbbae6f4f9260be7cabd99591e7f107bff
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-06-21 12:52:42 +0800

    Delete acosta-dance-centre-child directory

--- acosta-dance-centre-child/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-add_action( 'wp_enqueue_scripts', 'doro_add_stylesheet', 20);
-function doro_add_stylesheet() {
-	wp_enqueue_style( 'doro-child-style', get_stylesheet_directory_uri() . '/style.css', false, '1.0', 'all' );
-}
-?>

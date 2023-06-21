a6165c475cb526a5dd403a98160dbdc6c56c0d07
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-06-21 12:30:20 +0800

    changges

--- wp-content/plugins/doro-plugin/page-links-to.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Main plugin class.
-require( dirname( __FILE__ ) . '/classes/plugin.php' );
-
-// Functions.
-require( dirname( __FILE__ ) . '/inc/functions.php' );
-
-// Bootstrap everything.
-new CWS_PageLinksTo( __FILE__ );

0b60b867a765ae7da84aabb1513a07f5068ef3a2
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-18 07:10:14 +0800

    Update featured.php

--- themes/acosta-dance-centre-child/template-parts/woocommerce/featured.php
+++ themes/acosta-dance-centre-child/template-parts/woocommerce/featured.php
@@ -18,7 +18,7 @@
                 <span class="month">JULY</span>
               </div>
             </div>
-            <div class="col-lg-4">
+            <div class="col-lg-3">
               <div class="image-box">
                 <img src="<?= get_the_post_thumbnail_url($class['id']) ?>" alt="<?= get_the_title($class['id']) ?>">
               </div>
@@ -26,6 +26,11 @@
             <div class="col-lg-5">
               <div class="heading-box">
                 <h3><?= get_the_title($class['id']) ?></h3>
+              </div>
+            </div>
+            <div class="col-lg-2">
+              <div class="book-box">
+                <a href="<?= get_permalink($class['id']) ?>">BOOK NOW</a>
               </div>
             </div>
           </div>

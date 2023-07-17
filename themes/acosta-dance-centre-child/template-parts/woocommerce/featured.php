0d3b04af047cd4cbcf388059274552ddd3658b96
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-18 07:08:21 +0800

    Update featured.php

--- themes/acosta-dance-centre-child/template-parts/woocommerce/featured.php
+++ themes/acosta-dance-centre-child/template-parts/woocommerce/featured.php
@@ -1,15 +1,31 @@
-<section class="featured-product background-light-red md-padding-bottom d-flex">
-  <div class="inner">
-    <div class="heading-box">
-      <h2 class="big-title">
-        SUMMER 23<br> FEATURED CLASSES
-      </h2>
+<?php $featured_classes = get__theme_option('featured_classes'); ?>
+
+<?php if ($featured_classes) { ?>
+  <section class="featured-product background-light-red md-padding-bottom d-flex">
+    <div class="inner">
+      <div class="heading-box">
+        <h2 class="big-title">
+          SUMMER 23<br> FEATURED CLASSES
+        </h2>
+      </div>
+
+      <div class="product-box-style-1">.
+        <?php foreach ($featured_classes as $class) { ?>
+          <div class="row">
+            <div class="col-lg-2">
+              <div class="meta-date">
+                <span class="day">25</span>
+                <span class="month">JULY</span>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="image-box">
+                <img src="<?= get_the_post_thumbnail_url($class['id']) ?>" alt="<?= get_the_title($class['id']) ?>">
+              </div>
+            </div>
+          </div>
+        <?php } ?>
+      </div>
     </div>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-  </div>
-</section>
+  </section>
+<?php } ?>

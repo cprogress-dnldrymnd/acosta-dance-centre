0e7b78d62f3e9498d9959892fc712bfd8e495026
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-18 06:56:13 +0800

    Update featured.php

--- themes/acosta-dance-centre-child/template-parts/woocommerce/featured.php
+++ themes/acosta-dance-centre-child/template-parts/woocommerce/featured.php
@@ -1,13 +1,15 @@
 <section class="featured-product background-light-red md-padding">
-  <div class="heading-box">
-    <h2 class="big-title">
-      SUMMER 23<br> FEATURED CLASSES
-    </h2>
+  <div class="inner">
+    <div class="heading-box">
+      <h2 class="big-title">
+        SUMMER 23<br> FEATURED CLASSES
+      </h2>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
   </div>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
 </section>

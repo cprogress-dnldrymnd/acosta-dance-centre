88b6c48cc6989f8918a9626a8e2e5324ce96d70d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-18 03:17:56 +0800

    Update functions.php

--- themes/acosta-dance-centre-child/functions.php
+++ themes/acosta-dance-centre-child/functions.php
@@ -1,5 +1,5 @@
 <?php
-add_action('wp_enqueue_scripts', 'doro_add_stylesheet', 20);
+add_action('wp_enqueue_scripts', 'doro_add_stylesheet', 30);
 function doro_add_stylesheet()
 {
 	wp_enqueue_style('doro-child-style', get_stylesheet_directory_uri() . '/style.css', false, '1.0', 'all');

c01d3c0c84e5a9cb6f5094796a4d4bf6dba6ff93
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-18 07:18:42 +0800

    changes

--- themes/acosta-dance-centre-child/template-parts/woocommerce/featured.php
+++ themes/acosta-dance-centre-child/template-parts/woocommerce/featured.php
@@ -11,26 +11,28 @@
 
       <div class="product-box-style-1">
         <?php foreach ($featured_classes as $class) { ?>
-          <div class="row align-items-center">
-            <div class="col-lg-2 text-center">
-              <div class="meta-date">
-                <span class="day d-block">25</span>
-                <span class="month d-block">JULY</span>
+          <div class="product-box-item">
+            <div class="row align-items-center">
+              <div class="col-lg-2 text-center">
+                <div class="meta-date">
+                  <span class="day d-block">25</span>
+                  <span class="month d-block">JULY</span>
+                </div>
               </div>
-            </div>
-            <div class="col-lg-3">
-              <div class="image-box position-relative">
-                <img src="<?= get_the_post_thumbnail_url($class['id']) ?>" alt="<?= get_the_title($class['id']) ?>">
+              <div class="col-lg-3">
+                <div class="image-box position-relative">
+                  <img src="<?= get_the_post_thumbnail_url($class['id']) ?>" alt="<?= get_the_title($class['id']) ?>">
+                </div>
               </div>
-            </div>
-            <div class="col-lg-5">
-              <div class="heading-box">
-                <h3><?= get_the_title($class['id']) ?></h3>
+              <div class="col-lg-5">
+                <div class="heading-box">
+                  <h3><?= get_the_title($class['id']) ?></h3>
+                </div>
               </div>
-            </div>
-            <div class="col-lg-2">
-              <div class="book-box">
-                <a href="<?= get_permalink($class['id']) ?>">BOOK</a>
+              <div class="col-lg-2">
+                <div class="book-box">
+                  <a href="<?= get_permalink($class['id']) ?>">BOOK</a>
+                </div>
               </div>
             </div>
           </div>

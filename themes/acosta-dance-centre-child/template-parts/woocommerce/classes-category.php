65f6d0940d6d9e162479e7c7043522bf797b66f1
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-18 06:31:38 +0800

    Update classes-category.php

--- themes/acosta-dance-centre-child/template-parts/woocommerce/classes-category.php
+++ themes/acosta-dance-centre-child/template-parts/woocommerce/classes-category.php
@@ -2,11 +2,6 @@
 $term_id = get_queried_object()->term_id;
 $product_category = get__terms('product_cat', $term_id);
 ?>
-<pre>
-  <?php
-  var_dump(get__terms('product_cat', $term_id));
-  ?>
-</pre>
 <?php if ($product_category) { ?>
   <div class="product-category-holder">
     <div class="row">

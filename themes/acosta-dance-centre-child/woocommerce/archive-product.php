75a4980a0efb056b16e8d70456ac1919166f1293
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-18 07:25:40 +0800

    Update archive-product.php

--- themes/acosta-dance-centre-child/woocommerce/archive-product.php
+++ themes/acosta-dance-centre-child/woocommerce/archive-product.php
@@ -62,55 +62,60 @@ if ($is_classes) {
 
 }
 ?>
-<div class="sec-title text-left">
-	<span class="heading-meta">FIND A CLASS</span>
-	<h2 class="doro-heading">BOOK A CLASS TODAY</h2>
-</div>
+
+<section class="product-loop md-padding">
+	<div class="sec-title text-left">
+		<span class="heading-meta">FIND A CLASS</span>
+		<h2 class="doro-heading">BOOK A CLASS TODAY</h2>
+	</div>
+	<?php
+	if (woocommerce_product_loop()) {
+
+		/**
+		 * Hook: woocommerce_before_shop_loop.
+		 *
+		 * @hooked woocommerce_output_all_notices - 10
+		 * @hooked woocommerce_result_count - 20
+		 * @hooked woocommerce_catalog_ordering - 30
+		 */
+		do_action('woocommerce_before_shop_loop');
+
+		woocommerce_product_loop_start();
+
+		if (wc_get_loop_prop('total')) {
+			while (have_posts()) {
+				the_post();
+
+				/**
+				 * Hook: woocommerce_shop_loop.
+				 */
+				do_action('woocommerce_shop_loop');
+
+				wc_get_template_part('content', 'product');
+			}
+		}
+
+		woocommerce_product_loop_end();
+
+		/**
+		 * Hook: woocommerce_after_shop_loop.
+		 *
+		 * @hooked woocommerce_pagination - 10
+		 */
+		do_action('woocommerce_after_shop_loop');
+	}
+	else {
+		/**
+		 * Hook: woocommerce_no_products_found.
+		 *
+		 * @hooked wc_no_products_found - 10
+		 */
+		do_action('woocommerce_no_products_found');
+	}
+	?>
+</section>
 <?php
 
-if (woocommerce_product_loop()) {
-
-	/**
-	 * Hook: woocommerce_before_shop_loop.
-	 *
-	 * @hooked woocommerce_output_all_notices - 10
-	 * @hooked woocommerce_result_count - 20
-	 * @hooked woocommerce_catalog_ordering - 30
-	 */
-	do_action('woocommerce_before_shop_loop');
-
-	woocommerce_product_loop_start();
-
-	if (wc_get_loop_prop('total')) {
-		while (have_posts()) {
-			the_post();
-
-			/**
-			 * Hook: woocommerce_shop_loop.
-			 */
-			do_action('woocommerce_shop_loop');
-
-			wc_get_template_part('content', 'product');
-		}
-	}
-
-	woocommerce_product_loop_end();
-
-	/**
-	 * Hook: woocommerce_after_shop_loop.
-	 *
-	 * @hooked woocommerce_pagination - 10
-	 */
-	do_action('woocommerce_after_shop_loop');
-}
-else {
-	/**
-	 * Hook: woocommerce_no_products_found.
-	 *
-	 * @hooked wc_no_products_found - 10
-	 */
-	do_action('woocommerce_no_products_found');
-}
 
 /**
  * Hook: woocommerce_after_main_content.

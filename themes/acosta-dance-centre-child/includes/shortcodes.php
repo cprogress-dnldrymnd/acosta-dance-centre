7aeb4edd67a77f18ceabe59b8857aa7344dec020
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-21 22:41:04 +0800

    changes

---
+++ themes/acosta-dance-centre-child/includes/shortcodes.php
@@ -0,0 +1,60 @@
+
+function events()
+{
+	ob_start();
+	$args = array(
+		'posts_per_page' => 3,
+		'post_type'      => 'tc_events'
+	);
+	$query = new WP_Query($args);
+	?>
+	<div class="events-holder">
+		<div class="row">
+			<?php while ($query->have_posts()) { ?>
+				<?php
+				$query->the_post();
+				$event_date_time = get_post_meta(get_the_ID(), 'event_date_time', true);
+				$dateformat = strtotime($event_date_time);
+				//echo $ticket_date = date('D j M Y', $dateformat) . '<br>';
+				//echo $ticket_time = date('g:i a', $dateformat);
+		
+				$event_month = date('M', $dateformat);
+				$event_day = date('j', $dateformat);
+				$event_time = date('g:i a', $dateformat);
+
+				?>
+				<div class="col-lg-4">
+					<pre class="d-none"><?php var_dump(get_post_meta(get_the_ID())) ?></pre>
+					<div class="event-box">
+						<a href="<?php the_permalink() ?>">
+							<div class="image-box mb-3">
+								<img src="<?= get_the_post_thumbnail_url(get_the_ID(), 'large') ?>" alt="">
+							</div>
+
+							<div class="title-box d-flex">
+								<div class="month-day d-flex flex-column justify-content-between">
+									<span class="day d-block"><?= $event_day ?></span>
+									<span class="month d-block text-uppercase"><?= $event_month ?></span>
+								</div>
+								<div class="title-time d-flex flex-column">
+									<h3>
+										<?php the_title() ?>
+									</h3>
+									<span class="time text-uppercase d-block">
+										<?= $event_time ?>
+									</span>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+			<?php } ?>
+			<?php wp_reset_postdata() ?>
+		</div>
+	</div>
+	<?php
+
+	return ob_get_clean();
+}
+
+add_shortcode('events', 'events');

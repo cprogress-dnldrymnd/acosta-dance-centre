229cc1c168a3964287bdd36760d42bd36765128a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-21 22:47:40 +0800

    cha

--- themes/acosta-dance-centre-child/template-parts/woocommerce/featured.php
+++ themes/acosta-dance-centre-child/template-parts/woocommerce/featured.php
@@ -30,7 +30,7 @@
               <div class="col-lg-5">
                 <div class="title-box">
                   <h3><?= get_the_title($class['id']) ?></h3>
-                  <span class="time d-block"><?= $ticket_time ?></span>
+                  <span class="time meta-style-1 d-block text-uppercase">TIME: <?= $ticket_time ?></span>
                 </div>
               </div>
               <div class="col-lg-2">

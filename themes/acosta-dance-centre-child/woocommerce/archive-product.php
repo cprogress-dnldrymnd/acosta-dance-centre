60f4a082054bc9de6974e8ee907ee84de119c512
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-21 22:52:42 +0800

    ch

--- themes/acosta-dance-centre-child/woocommerce/archive-product.php
+++ themes/acosta-dance-centre-child/woocommerce/archive-product.php
@@ -58,8 +58,8 @@ $is_classes = get__term_meta(get_queried_object()->term_id, 'is_classes');
 <?php
 if ($is_classes) {
 	get_template_part('template-parts/woocommerce/classes-category');
-	get_template_part('template-parts/woocommerce/featured');
-
+	$featured_classes = get__theme_option('featured_classes');
+	include 'template-parts/usable/classes-category';
 }
 ?>
 

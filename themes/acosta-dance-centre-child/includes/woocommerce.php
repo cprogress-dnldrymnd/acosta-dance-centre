a8a70dc4d9757e2c573ace11cddb5ffb85c4f1f5
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-24 17:25:41 +0800

    Update woocommerce.php

--- themes/acosta-dance-centre-child/includes/woocommerce.php
+++ themes/acosta-dance-centre-child/includes/woocommerce.php
@@ -17,24 +17,26 @@ function woocommerce_add_to_cart_button_text_archives()
 remove_action('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10);
 
 
-function action_woocommerce_before_shop_loop_item_title()
+function action_woocommerce_before_shop_loop_item()
 {
   $ticket_date_val = get_post_meta(get_the_ID(), '_ticket_checkin_availability_from_date', true);
   $dateformat = strtotime($ticket_date_val);
   $ticket_time = date('g:i a', $dateformat);
+  echo ' <div class="image-holder">';
   ?>
   <div class="event-date">
   </div>
   <?php
 }
 
-add_action('woocommerce_before_shop_loop_item', 'action_woocommerce_before_shop_loop_item_title');
+add_action('woocommerce_before_shop_loop_item', 'action_woocommerce_before_shop_loop_item');
 
 function action_woocommerce_after_shop_loop_item_title()
 {
+  echo '</div>';
   ?>
   <h3>
-xzxx
+    <?= get_the_title() ?>
   </h3>
   <?php
 }

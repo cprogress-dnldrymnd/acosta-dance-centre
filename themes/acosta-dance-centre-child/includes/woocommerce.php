806d077dba4a689221c677825508a0f87eadb193
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-24 17:13:16 +0800

    Update woocommerce.php

--- themes/acosta-dance-centre-child/includes/woocommerce.php
+++ themes/acosta-dance-centre-child/includes/woocommerce.php
@@ -15,3 +15,11 @@ function woocommerce_add_to_cart_button_text_archives()
  */
 
 remove_action('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10);
+
+
+function action_woocommerce_before_shop_loop_item_title()
+{
+  echo 'ttes';
+}
+
+add_action('woocommerce_before_shop_loop_item_title', 'woocommerce_before_shop_loop_item_title');

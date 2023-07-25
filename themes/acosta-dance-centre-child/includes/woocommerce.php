03b7401166625aa6bf3e66b6ab70a04814f738cd
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-25 12:03:56 +0800

    Update woocommerce.php

--- themes/acosta-dance-centre-child/includes/woocommerce.php
+++ themes/acosta-dance-centre-child/includes/woocommerce.php
@@ -124,28 +124,13 @@ function conditionaly_removing_product_tabs($tabs)
 
 function action_woocommerce_after_single_product()
 {
+  global $product;
   echo '</div>';
   echo ' <div class="col-lg-4">';
   ?>
   <div class="details-box">
     <h3 class="doro-heading">DETAILS</h3>
-    <?php
-    if (!$product_attributes) {
-      return;
-    }
-    ?>
-    <table class="woocommerce-product-attributes shop_attributes">
-      <?php foreach ($product_attributes as $product_attribute_key => $product_attribute): ?>
-        <tr
-          class="woocommerce-product-attributes-item woocommerce-product-attributes-item--<?php echo esc_attr($product_attribute_key); ?>">
-          <th class="woocommerce-product-attributes-item__label"><?php echo wp_kses_post($product_attribute['label']); ?>
-          </th>
-          <td class="woocommerce-product-attributes-item__value"><?php echo wp_kses_post($product_attribute['value']); ?>
-          </td>
-        </tr>
-      <?php endforeach; ?>
-    </table>
-
+    <?php do_action('woocommerce_product_additional_information', $product); ?>
   </div>
   <?php
   echo '</div>';

03cdea9eb5522cd3f06888f7ef1e5b35c46fc3d4
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-25 17:32:51 +0800

    Update woocommerce.php

--- themes/acosta-dance-centre-child/includes/woocommerce.php
+++ themes/acosta-dance-centre-child/includes/woocommerce.php
@@ -303,4 +303,4 @@ function default_no_quantities( $individually, $product ){
 $individually = true;
 return $individually;
 }
-add_filter( 'woocommerce_is_sold_individually', 'default_no_quantities', 10, 2 )
+add_filter( 'woocommerce_is_sold_individually', 'default_no_quantities', 10, 2 );

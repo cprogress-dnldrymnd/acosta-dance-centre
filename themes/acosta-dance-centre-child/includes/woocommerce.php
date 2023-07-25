6901b0c5c08e8a5b93ba1bbda6f51ef5dbd72eed
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-26 00:14:10 +0800

    Update woocommerce.php

--- themes/acosta-dance-centre-child/includes/woocommerce.php
+++ themes/acosta-dance-centre-child/includes/woocommerce.php
@@ -217,7 +217,7 @@ function action_woocommerce_after_single_product()
         <a href="/category/tickets/classes/" class="w-100">FIND A DANCE CLASS</a>
       </div>
       <div class="button-box button-bordered">
-        <a href="/category/tickets/classes/" class="w-100">FIND A WORKSHOP</a>
+        <a href="/category/tickets/workshops/" class="w-100">FIND A WORKSHOP</a>
       </div>
     </div>
   <?php } ?>

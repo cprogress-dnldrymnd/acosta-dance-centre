4ac6f2c70b41142ac576b8f87a2baa182090a569
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-26 19:13:28 +0800

    Update loop-classes.php

--- themes/acosta-dance-centre-child/template-parts/usable/loop-classes.php
+++ themes/acosta-dance-centre-child/template-parts/usable/loop-classes.php
@@ -63,7 +63,7 @@ else {
 }
 ?>
 
-<?php if ($args['type'] == 'related') { ?>
+<?php if ($more_button_val) { ?>
   <div class="more-button text-right d-none d-lg-block">
     <a class="d-inline-flex align-items-center" href="<?= get_term_link($categ_id) ?>">
       <span class="text mr-3"><?= $more_text ?></span>
@@ -137,7 +137,7 @@ else {
         </div>
       <?php } ?>
     </div>
-    <?php if ($args['type'] == 'related') { ?>
+    <?php if ($more_button_val) { ?>
       <div class="more-button  d-block d-lg-none text-center pb-5">
         <a class="d-inline-flex align-items-center" href="<?= get_term_link($categ_id) ?>">
           <span class="text mr-3"><?= $more_text ?></span>

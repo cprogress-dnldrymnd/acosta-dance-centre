0d5e96f0bf6bb69dd3bd635b7bc7fdc788dfc970
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-26 18:27:47 +0800

    Update loop-classes.php

--- themes/acosta-dance-centre-child/template-parts/usable/loop-classes.php
+++ themes/acosta-dance-centre-child/template-parts/usable/loop-classes.php
@@ -125,7 +125,7 @@ else {
       <?php } ?>
     </div>
     <?php if ($args['type'] == 'related') { ?>
-      <div class="more-button text-right">
+      <div class="more-button text-center">
         <a class="d-inline-flex align-items-center" href="<?= get_term_link($categ_id) ?>">
           <span class="text mr-3"><?= $more_text ?></span>
           <span class="icon">

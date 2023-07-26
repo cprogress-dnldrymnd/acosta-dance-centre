0b701842a8a1fd18f5a5c5fc689e2d6b2b35376a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-27 03:55:15 +0800

    Update header.php

--- themes/acosta-dance-centre-child/header.php
+++ themes/acosta-dance-centre-child/header.php
@@ -8,6 +8,7 @@
 		content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<meta name="robots" content="index, follow" />
 	<?php wp_head(); ?>
+
 </head>
 
 <body <?php body_class(); ?>>
@@ -32,7 +33,7 @@
 			</svg>
 		</div>
 		<?php wp_enqueue_script('doro-scrollto-script'); ?>
-	<?php
+		<?php
 	}
 	?>
 	<div id="doro-page"> <a href="#" class="js-doro-nav-toggle doro-nav-toggle"><i></i></a>

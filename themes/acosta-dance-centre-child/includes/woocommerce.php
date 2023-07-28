62b367c871bb4726c5be5784d3af4713c2250e99
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-07-28 20:45:46 +0800

    Update woocommerce.php

--- themes/acosta-dance-centre-child/includes/woocommerce.php
+++ themes/acosta-dance-centre-child/includes/woocommerce.php
@@ -392,9 +392,17 @@ function action_woocommerce_cart_totals_before_shipping()
   if (!empty($memberships)) {
     // do something for this active member
     ?>
+    <tr>
+      <td colspan=2>
+        <pre>
+              <?php var_dump($memberships) ?>
+            </pre>
+      </td>
+    </tr>
     <tr class='cart-subtotal'>
       <th>Membership Discount</th>
       <td>
+
         <?php foreach ($memberships as $membership) { ?>
           <span class="membership"><?= $membership->name ?></span>
         <?php } ?>

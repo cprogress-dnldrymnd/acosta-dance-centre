2711e92e2bfb60ba3b88b10ed01990374efc3b64
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-08-03 23:50:39 +0800

    Update loop-classes.php

--- themes/acosta-dance-centre-child/template-parts/usable/loop-classes.php
+++ themes/acosta-dance-centre-child/template-parts/usable/loop-classes.php
@@ -1,21 +1,4 @@
 <?php
-$meta_query = [];
-
-$meta_query[] = [
-  'key'     => '_ticket_checkin_availability_from_date',
-  'value'   => date('Y/m/d'),
-  'compare' => '>=',
-  'type'    => 'DATE'
-];
-
-$posts = array(
-  'post_type'      => 'product',
-  'posts_per_page' => 3,
-  'meta_query'     => $meta_query,
-  'orderby'        => 'meta_value',
-  'order'          => 'ASC'
-);
-
 if ($args['type'] == 'featured_classes') {
   $classes = get__theme_option('featured_classes');
 
@@ -28,16 +11,32 @@ else if ($args['type'] == 'related') {
   $terms = get_the_terms(get_the_ID(), 'product_cat');
   $terms_val = array();
 
+
   foreach ($terms as $term) {
     $terms_val[] = $term->term_id;
   }
+  $meta_query = [];
 
-  $posts[]['tax_query'] = array(
-    array(
-      'taxonomy' => 'product_cat',
-      'field'    => 'term_id',
-      'terms'    => $terms_val
-    )
+  $meta_query[] = [
+    'key'     => '_ticket_checkin_availability_from_date',
+    'value'   => date('Y/m/d'),
+    'compare' => '>=',
+    'type'    => 'DATE'
+  ];
+
+  $posts = array(
+    'post_type'      => 'product',
+    'posts_per_page' => 3,
+    'tax_query'      => array(
+      array(
+        'taxonomy' => 'product_cat',
+        'field'    => 'term_id',
+        'terms'    => $terms_val
+      )
+    ),
+    'meta_query'     => $meta_query,
+    'orderby'        => 'meta_value',
+    'order'          => 'ASC'
   );
 
 
@@ -61,15 +60,6 @@ else if ($args['type'] == 'related') {
 else {
   $featured_classes = get__theme_option('featured_classes');
 }
-
-foreach ($post_list as $post) {
-  $classes[] = array(
-    'id' => $post->ID
-  );
-}
-
-
-
 /*
 if ($args['categ']) {
   $term_name = get_term($args['categ'])->name;

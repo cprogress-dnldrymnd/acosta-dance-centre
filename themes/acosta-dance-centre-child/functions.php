c4cf93745903888533e9fca514ce9f86aca71f56
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-22 17:59:33 +0800

    Update functions.php

--- themes/acosta-dance-centre-child/functions.php
+++ themes/acosta-dance-centre-child/functions.php
@@ -2,7 +2,9 @@
 add_action('wp_enqueue_scripts', 'doro_add_stylesheet', 30);
 function doro_add_stylesheet()
 {
+	wp_enqueue_style('doro-swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css');
 	wp_enqueue_style('doro-child-style', get_stylesheet_directory_uri() . '/style.css', false, '1.0', 'all');
+	wp_enqueue_script('doro-swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js');
 }
 
 /*-----------------------------------------------------------------------------------*/
@@ -74,7 +76,6 @@ function action_widgets_init()
 			'after_title'   => '</h5>',
 		)
 	);
-
 }
 add_action('widgets_init', 'action_widgets_init');
 
@@ -82,95 +83,78 @@ function social_network()
 {
 	$doro_options = get_option('doro');
 	ob_start();
-	?>
+?>
 	<?php if (Doro_AfterSetupTheme::return_thme_option('social-footer') != 'no') { ?>
 		<ul class="social-network">
-			<?php if (!empty($doro_options['facebook'])): ?>
-				<li><a href="<?php echo esc_url($doro_options['facebook']); ?>" target="_blank"><i
-							class="ti-facebook font-14px black-icon"></i></a></li>
+			<?php if (!empty($doro_options['facebook'])) : ?>
+				<li><a href="<?php echo esc_url($doro_options['facebook']); ?>" target="_blank"><i class="ti-facebook font-14px black-icon"></i></a></li>
 			<?php endif; ?>
 
-			<?php if (!empty($doro_options['twitter'])): ?>
-				<li><a href="<?php echo esc_url($doro_options['twitter']); ?>" target="_blank"><i
-							class="ti-twitter-alt font-14px black-icon"></i></a></li>
+			<?php if (!empty($doro_options['twitter'])) : ?>
+				<li><a href="<?php echo esc_url($doro_options['twitter']); ?>" target="_blank"><i class="ti-twitter-alt font-14px black-icon"></i></a></li>
 			<?php endif; ?>
 
-			<?php if (!empty($doro_options['google-plus'])): ?>
-				<li><a href="<?php echo esc_url($doro_options['google-plus']); ?>" target="_blank"><i
-							class="ti-google font-14px black-icon"></i></a></li>
+			<?php if (!empty($doro_options['google-plus'])) : ?>
+				<li><a href="<?php echo esc_url($doro_options['google-plus']); ?>" target="_blank"><i class="ti-google font-14px black-icon"></i></a></li>
 			<?php endif; ?>
 
-			<?php if (!empty($doro_options['linkedin'])): ?>
-				<li><a href="<?php echo esc_url($doro_options['linkedin']); ?>" target="_blank"><i
-							class="ti-linkedin font-14px black-icon"></i></a></li>
+			<?php if (!empty($doro_options['linkedin'])) : ?>
+				<li><a href="<?php echo esc_url($doro_options['linkedin']); ?>" target="_blank"><i class="ti-linkedin font-14px black-icon"></i></a></li>
 			<?php endif; ?>
 
-			<?php if (!empty($doro_options['instagram'])): ?>
-				<li><a href="<?php echo esc_url($doro_options['instagram']); ?>" target="_blank"><i
-							class="ti-instagram font-14px black-icon"></i></a></li>
+			<?php if (!empty($doro_options['instagram'])) : ?>
+				<li><a href="<?php echo esc_url($doro_options['instagram']); ?>" target="_blank"><i class="ti-instagram font-14px black-icon"></i></a></li>
 			<?php endif; ?>
 
-			<?php if (!empty($doro_options['pinterest'])): ?>
-				<li><a href="<?php echo esc_url($doro_options['pinterest']); ?>" target="_blank"><i
-							class="ti-pinterest-alt font-14px black-icon"></i></a></li>
+			<?php if (!empty($doro_options['pinterest'])) : ?>
+				<li><a href="<?php echo esc_url($doro_options['pinterest']); ?>" target="_blank"><i class="ti-pinterest-alt font-14px black-icon"></i></a></li>
 			<?php endif; ?>
 
-			<?php if (!empty($doro_options['soundcloud'])): ?>
-				<li><a href="<?php echo esc_url($doro_options['soundcloud']); ?>" target="_blank"><i
-							class="ti-soundcloud font-14px black-icon"></i></a></li>
+			<?php if (!empty($doro_options['soundcloud'])) : ?>
+				<li><a href="<?php echo esc_url($doro_options['soundcloud']); ?>" target="_blank"><i class="ti-soundcloud font-14px black-icon"></i></a></li>
 			<?php endif; ?>
 
-			<?php if (!empty($doro_options['skype'])): ?>
-				<li><a href="<?php echo esc_url($doro_options['skype']); ?>" target="_blank"><i
-							class="ti-skype font-14px black-icon"></i></a></li>
+			<?php if (!empty($doro_options['skype'])) : ?>
+				<li><a href="<?php echo esc_url($doro_options['skype']); ?>" target="_blank"><i class="ti-skype font-14px black-icon"></i></a></li>
 			<?php endif; ?>
 
-			<?php if (!empty($doro_options['dribbble'])): ?>
-				<li><a href="<?php echo esc_url($doro_options['dribbble']); ?>" target="_blank"><i
-							class="ti-dribbble font-14px black-icon"></i></a></li>
+			<?php if (!empty($doro_options['dribbble'])) : ?>
+				<li><a href="<?php echo esc_url($doro_options['dribbble']); ?>" target="_blank"><i class="ti-dribbble font-14px black-icon"></i></a></li>
 			<?php endif; ?>
 
-			<?php if (!empty($doro_options['youtube'])): ?>
-				<li><a href="<?php echo esc_url($doro_options['youtube']); ?>" target="_blank"><i
-							class="ti-youtube font-14px black-icon"></i></a></li>
+			<?php if (!empty($doro_options['youtube'])) : ?>
+				<li><a href="<?php echo esc_url($doro_options['youtube']); ?>" target="_blank"><i class="ti-youtube font-14px black-icon"></i></a></li>
 			<?php endif; ?>
 
-			<?php if (!empty($doro_options['vimeo'])): ?>
-				<li><a href="<?php echo esc_url($doro_options['vimeo']); ?>" target="_blank"><i
-							class="ti-vimeo font-14px black-icon"></i></a></li>
+			<?php if (!empty($doro_options['vimeo'])) : ?>
+				<li><a href="<?php echo esc_url($doro_options['vimeo']); ?>" target="_blank"><i class="ti-vimeo font-14px black-icon"></i></a></li>
 			<?php endif; ?>
 
-			<?php if (!empty($doro_options['dropbox'])): ?>
-				<li><a href="<?php echo esc_url($doro_options['dropbox']); ?>" target="_blank"><i
-							class="ti-dropbox font-14px black-icon"></i></a></li>
+			<?php if (!empty($doro_options['dropbox'])) : ?>
+				<li><a href="<?php echo esc_url($doro_options['dropbox']); ?>" target="_blank"><i class="ti-dropbox font-14px black-icon"></i></a></li>
 			<?php endif; ?>
 
-			<?php if (!empty($doro_options['github'])): ?>
-				<li><a href="<?php echo esc_url($doro_options['github']); ?>" target="_blank"><i
-							class="ti-github font-14px black-icon"></i></a></li>
+			<?php if (!empty($doro_options['github'])) : ?>
+				<li><a href="<?php echo esc_url($doro_options['github']); ?>" target="_blank"><i class="ti-github font-14px black-icon"></i></a></li>
 			<?php endif; ?>
-			<?php if (!empty($doro_options['tumblr'])): ?>
-				<li><a href="<?php echo esc_url($doro_options['tumblr']); ?>" target="_blank"><i
-							class="ti-tumblr-alt font-14px black-icon"></i></a></li>
+			<?php if (!empty($doro_options['tumblr'])) : ?>
+				<li><a href="<?php echo esc_url($doro_options['tumblr']); ?>" target="_blank"><i class="ti-tumblr-alt font-14px black-icon"></i></a></li>
 			<?php endif; ?>
 
-			<?php if (!empty($doro_options['email'])): ?>
-				<li><a href="mailto:<?php echo esc_attr($doro_options['email']); ?>" target="_blank"><i
-							class="ti-email font-14px black-icon"></i></a></li>
+			<?php if (!empty($doro_options['email'])) : ?>
+				<li><a href="mailto:<?php echo esc_attr($doro_options['email']); ?>" target="_blank"><i class="ti-email font-14px black-icon"></i></a></li>
 			<?php endif; ?>
 
 			<?php
 			$doro_more_social = Doro_AfterSetupTheme::return_thme_option('opt_add_more_social', '');
 			if (!empty($doro_more_social)) {
-				foreach ($doro_more_social as $doro_more_socials) {
-					; ?>
+				foreach ($doro_more_social as $doro_more_socials) {; ?>
 					<?php echo balanceTags($doro_more_socials); ?>
-				<?php }
-			}
-			; ?>
+			<?php }
+			}; ?>
 		</ul>
 	<?php } ?>
-	<?php
+<?php
 
 	return ob_get_clean();
 }
@@ -193,8 +177,7 @@ function get__post_meta($value)
 {
 	if (function_exists('carbon_get_the_post_meta')) {
 		return carbon_get_the_post_meta($value);
-	}
-	else {
+	} else {
 		return 'Error: Carbonfield not activated';
 	}
 }
@@ -203,8 +186,7 @@ function get__term_meta($term_id, $value)
 {
 	if (function_exists('carbon_get_term_meta')) {
 		return carbon_get_term_meta($term_id, $value);
-	}
-	else {
+	} else {
 		return 'Error: Carbonfield not activated';
 	}
 }
@@ -213,8 +195,7 @@ function get__post_meta_by_id($id, $value)
 {
 	if (function_exists('carbon_get_post_meta')) {
 		return carbon_get_post_meta($id, $value);
-	}
-	else {
+	} else {
 		return 'Error: Carbonfield not activated';
 	}
 }
@@ -222,8 +203,7 @@ function get__theme_option($value)
 {
 	if (function_exists('carbon_get_theme_option')) {
 		return carbon_get_theme_option($value);
-	}
-	else {
+	} else {
 		return 'Error: Carbonfield not activated';
 	}
 }
@@ -353,7 +333,7 @@ function translate_text($translated)
 
 function action_admin_head()
 {
-	?>
+?>
 	<style>
 		#toplevel_page_doro {
 			display: none !important;
@@ -363,7 +343,7 @@ function action_admin_head()
 			display: none;
 		}
 	</style>
-	<?php
+<?php
 }
 
 add_action('admin_head', 'action_admin_head');
@@ -385,30 +365,23 @@ function single_get_date($id, $type = 'date')
 		if ($type == 'date') {
 			if ($ticket_date_from != $ticket_date_to) {
 				return $ticket_date_from . ' - ' . $ticket_date_to . ' ' . $ticket_date_Y;
+			} else {
+				return $ticket_date_from . ' ' . $ticket_date_Y;;
 			}
-			else {
-				return $ticket_date_from . ' ' . $ticket_date_Y;
-				;
-			}
-		}
-		else {
+		} else {
 			if ($ticket_time_from != $ticket_time_to) {
 				return $ticket_time_from . ' - ' . $ticket_time_to;
-			}
-			else {
+			} else {
 				return $ticket_time_from;
 			}
 		}
-	}
-	else {
+	} else {
 		if ($type == 'date') {
 			return $ticket_date_from;
-		}
-		else {
+		} else {
 			return $ticket_time_from;
 		}
 	}
-
 }
 
 function update_product_time_attribute($product_id)

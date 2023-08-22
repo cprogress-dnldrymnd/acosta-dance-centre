de5e213b86f637d7d587924b221a27c4fcf92ee6
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-22 17:57:05 +0800

    Update pa_studio.php

--- themes/acosta-dance-centre-child/template-parts/woocommerce/pa_studio.php
+++ themes/acosta-dance-centre-child/template-parts/woocommerce/pa_studio.php
@@ -20,52 +20,53 @@ $content = get__term_meta(get_queried_object()->term_id, 'content');
     <div class="row">
         <div class="col-xl-8">
             <div class="studio-gallery">
-                <div class="content-margin">
 
-                    <div class="content-box">
-                        <?= wpautop($content) ?>
-                    </div>
+            </div>
+            <div class="content-margin">
 
-                    <div class="book-now-wrapper text-center">
-                        <div class="button-group-box text-center justify-content-center " id="book-now">
-                            <div class="button-box button-black">
+                <div class="content-box">
+                    <?= wpautop($content) ?>
+                </div>
 
-                                <form class="cart" action="https://acostadancecentre.com/product/ballet-class-with-sue-trevor-3/" method="post" enctype="multipart/form-data">
+                <div class="book-now-wrapper text-center">
+                    <div class="button-group-box text-center justify-content-center " id="book-now">
+                        <div class="button-box button-black">
 
-                                    <div class="quantity">
-                                        <label class="screen-reader-text" for="quantity_64e483aee24f7">Ballet Class Roland Price quantity</label>
-                                        <input type="hidden" id="quantity_64e483aee24f7" class="input-text qty text" name="quantity" value="1" aria-label="Product quantity" size="4" min="1" max="1" step="1" placeholder="" inputmode="numeric" autocomplete="off">
-                                    </div>
+                            <form class="cart" action="https://acostadancecentre.com/product/ballet-class-with-sue-trevor-3/" method="post" enctype="multipart/form-data">
 
-                                    <button type="submit" name="add-to-cart" value="913" class="single_add_to_cart_button alt">BOOK NOW</button>
+                                <div class="quantity">
+                                    <label class="screen-reader-text" for="quantity_64e483aee24f7">Ballet Class Roland Price quantity</label>
+                                    <input type="hidden" id="quantity_64e483aee24f7" class="input-text qty text" name="quantity" value="1" aria-label="Product quantity" size="4" min="1" max="1" step="1" placeholder="" inputmode="numeric" autocomplete="off">
+                                </div>
 
-                                </form>
+                                <button type="submit" name="add-to-cart" value="913" class="single_add_to_cart_button alt">BOOK NOW</button>
+
+                            </form>
 
 
-                            </div>
-                            <div class="button-box  button-bordered" id="join-now">
-                                <a href="/memberships">
-                                    MEMBERSHIPS
-                                </a>
-                            </div>
                         </div>
-                    </div>
-
-
-                    <div class="classes md-padding">
-                        <div class="more-button text-right d-none d-lg-block">
-                            <a class="d-inline-flex align-items-center" href="https://acostadancecentre.com/category/tickets/classes/">
-                                <span class="text mr-3">MORE CLASSES</span>
-                                <span class="icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="54.446" height="25.242" viewBox="0 0 54.446 25.242">
-                                        <g id="Icon_feather-arrow-right" data-name="Icon feather-arrow-right" transform="translate(1.5 2.121)">
-                                            <path id="Path_116" data-name="Path 116" d="M7.5,18H58.946" transform="translate(-7.5 -7.5)" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"></path>
-                                            <path id="Path_117" data-name="Path 117" d="M18,7.5,28.5,18,18,28.5" transform="translate(22.946 -7.5)" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"></path>
-                                        </g>
-                                    </svg>
-                                </span>
+                        <div class="button-box  button-bordered" id="join-now">
+                            <a href="/memberships">
+                                MEMBERSHIPS
                             </a>
                         </div>
+                    </div>
+                </div>
+
+
+                <div class="classes md-padding">
+                    <div class="more-button text-right d-none d-lg-block">
+                        <a class="d-inline-flex align-items-center" href="https://acostadancecentre.com/category/tickets/classes/">
+                            <span class="text mr-3">MORE CLASSES</span>
+                            <span class="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="54.446" height="25.242" viewBox="0 0 54.446 25.242">
+                                    <g id="Icon_feather-arrow-right" data-name="Icon feather-arrow-right" transform="translate(1.5 2.121)">
+                                        <path id="Path_116" data-name="Path 116" d="M7.5,18H58.946" transform="translate(-7.5 -7.5)" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"></path>
+                                        <path id="Path_117" data-name="Path 117" d="M18,7.5,28.5,18,18,28.5" transform="translate(22.946 -7.5)" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"></path>
+                                    </g>
+                                </svg>
+                            </span>
+                        </a>
                     </div>
                 </div>
             </div>

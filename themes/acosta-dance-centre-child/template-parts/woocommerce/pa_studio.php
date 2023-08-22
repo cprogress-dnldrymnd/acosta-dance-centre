4a3c8876ae705ed379fbe52c3742b8cff716a624
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-22 18:05:07 +0800

    ccc

--- themes/acosta-dance-centre-child/template-parts/woocommerce/pa_studio.php
+++ themes/acosta-dance-centre-child/template-parts/woocommerce/pa_studio.php
@@ -30,8 +30,16 @@ $gallery = get__term_meta(get_queried_object()->term_id, 'gallery');
                             </div>
                         <?php } ?>
                     </div>
-                    <div class="swiper-button-next"></div>
-                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next-custom">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+                        </svg>
+                    </div>
+                    <div class="swiper-button-prev-custom">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                        </svg>
+                    </div>
                 </div>
 
             </div>
@@ -130,8 +138,8 @@ get_footer();
 <script>
     var swiper = new Swiper(".mySwiperGallery", {
         navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
+            nextEl: ".swiper-button-next-custom",
+            prevEl: ".swiper-button-prev-custom",
         },
     });
 </script>

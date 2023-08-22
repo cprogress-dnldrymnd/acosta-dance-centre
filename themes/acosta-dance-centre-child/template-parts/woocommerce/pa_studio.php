2eadefddc27323c36070ff48aada605be2309c98
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-22 18:10:10 +0800

    Update pa_studio.php

--- themes/acosta-dance-centre-child/template-parts/woocommerce/pa_studio.php
+++ themes/acosta-dance-centre-child/template-parts/woocommerce/pa_studio.php
@@ -40,6 +40,8 @@ $gallery = get__term_meta(get_queried_object()->term_id, 'gallery');
                             <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
                         </svg>
                     </div>
+                    <div class="swiper-pagination"></div>
+
                 </div>
 
             </div>
@@ -141,6 +143,9 @@ get_footer();
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
         },
     });
 </script>

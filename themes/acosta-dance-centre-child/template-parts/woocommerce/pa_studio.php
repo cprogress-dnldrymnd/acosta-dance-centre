adabdf996477c5ddd0194f532e314011cfa2b85c
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-22 17:44:47 +0800

    Update pa_studio.php

--- themes/acosta-dance-centre-child/template-parts/woocommerce/pa_studio.php
+++ themes/acosta-dance-centre-child/template-parts/woocommerce/pa_studio.php
@@ -1,10 +1,18 @@
 <?php
 get_header();
 $name = get_queried_object()->name;
+$alt_title = get__term_meta(get_queried_object()->term_id, 'alt_title');
 ?>
 
 <nav class="woocommerce-breadcrumb">
-    <a href="https://acostadancecentre.com">Home</a>&nbsp;/&nbsp;Studio&nbsp;/&nbsp;<?= $name ?>
+    <a href="https://acostadancecentre.com">Home</a>&nbsp;/&nbsp;Studio&nbsp;/&nbsp;
+    <?php
+    if ($alt_title) {
+        echo $alt_title;
+    } else {
+        woocommerce_page_title();
+    }
+    ?>
 </nav>
 
 <?php

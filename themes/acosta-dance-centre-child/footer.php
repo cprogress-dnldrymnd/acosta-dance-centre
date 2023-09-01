8c1a61646ae8fe4460932f39ee7737c16b56c464
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-09-01 23:41:23 +0800

    cc

--- themes/acosta-dance-centre-child/footer.php
+++ themes/acosta-dance-centre-child/footer.php
@@ -79,7 +79,7 @@
               </div>
             <?php } ?>
         </div>
-        <div class="row">
+        <div class="row align-items-center">
           <div class="col-md-6">
             <?php dynamic_sidebar('footer_after_columns_left') ?>
           </div>

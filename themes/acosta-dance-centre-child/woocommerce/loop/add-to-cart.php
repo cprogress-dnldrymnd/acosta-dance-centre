fa951c1bcf44ec8482dc6f467e012199c996deef
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-09-04 21:42:52 +0800

    Update add-to-cart.php

--- themes/acosta-dance-centre-child/woocommerce/loop/add-to-cart.php
+++ themes/acosta-dance-centre-child/woocommerce/loop/add-to-cart.php
@@ -57,7 +57,7 @@ else { ?>
 				esc_attr(isset($args['quantity']) ? $args['quantity'] : 1),
 				esc_attr(isset($args['class']) ? $args['class'] : 'button'),
 				isset($args['attributes']) ? wc_implode_html_attributes($args['attributes']) : '',
-				esc_html($product->add_to_cart_text())
+				esc_html('ADD TO CART')
 			),
 			$product,
 			$args

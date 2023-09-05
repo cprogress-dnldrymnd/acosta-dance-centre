13fe955860f9e71c012ea3d7ab0092ddaa4b19ba
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-09-05 15:02:13 +0800

    Update page-calendar.php

--- themes/acosta-dance-centre-child/templates/page-calendar.php
+++ themes/acosta-dance-centre-child/templates/page-calendar.php
@@ -4,7 +4,35 @@
 /*-----------------------------------------------------------------------------------*/
 ?>
 <?php get_header(); ?>
-  <section class="calendar">
-    sdsdsdsd
-  </section>
+<section class="calendar">
+  <div id="calendar"></div>
+</section>
 <?php get_footer(); ?>
+<script>
+  var calendar = $("#calendar").calendarGC({
+    events: [
+      {
+        < a href = "https://www.jqueryscript.net/time-clock/" > date</a >: new Date("2022-02-07"),
+      eventName: "Holiday",
+      className: "my-class",
+      onclick(e, data) {
+        console.log(data);
+      },
+      dateColor: "red"
+      },
+    {
+      date: new Date("2022-02-07"),
+      eventName: "Holiday with wife",
+      className: "my-class",
+      onclick(e, data) {
+        console.log(data);
+      },
+      dateColor: "red"
+    },
+      // ... more events
+    ],
+  onclickDate: function (e, data) {
+    console.log(e, data);
+  }
+});
+</script>

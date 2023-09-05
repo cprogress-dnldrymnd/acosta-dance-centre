9b9d4b4bf934b8d6aedcf46f311547ebc630a0a7
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-09-05 15:23:09 +0800

    Update functions.php

--- themes/acosta-dance-centre-child/functions.php
+++ themes/acosta-dance-centre-child/functions.php
@@ -475,10 +475,10 @@ function mp_sync_on_product_save($product_id)
 function limit_string($string, $limit = 500)
 {
 	$string = strip_tags($string);
-	if (strlen($string) > 500) {
+	if (strlen($string) > $limit) {
 
 		// truncate string
-		$stringCut = substr($string, 0, 500);
+		$stringCut = substr($string, 0, $limit);
 		$endPoint = strrpos($stringCut, ' ');
 
 		//if the string doesn't contain any space then it will cut without word basis.

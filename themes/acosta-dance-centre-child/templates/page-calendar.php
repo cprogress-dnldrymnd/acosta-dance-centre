321a8f9578e5e6d927b113393586b684f6c8d2d3
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-09-05 15:08:24 +0800

    Update page-calendar.php

--- themes/acosta-dance-centre-child/templates/page-calendar.php
+++ themes/acosta-dance-centre-child/templates/page-calendar.php
@@ -6,9 +6,9 @@
 <?php get_header(); ?>
 <?php
 $args = array(
-  'post_type' => 'product',
+  'post_type'      => 'product',
   'posts_per_page' => -1,
-  'tax_query' => array(
+  'tax_query'      => array(
     array(
       'taxonomy' => 'product_cat',
       'field'    => 'slug',
@@ -17,6 +17,16 @@ $args = array(
   ),
 );
 $query = new WP_Query($args);
+if ($query->have_posts()) {
+  while ($query->have_posts()) {
+    $query->the_post();
+    echo '<pre>';
+    var_dump(get_post_meta(get_the_ID()));
+    echo '</pre>';
+  }
+  wp_reset_postdata();
+}
+
 ?>
 <section class="calendar">
   <div id="calendar"></div>

482c88aa0c55fdd28ac1e71b1623318d8d2b4490
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-09-05 18:15:59 +0800

    Update page-calendar.php

--- themes/acosta-dance-centre-child/templates/page-calendar.php
+++ themes/acosta-dance-centre-child/templates/page-calendar.php
@@ -57,7 +57,7 @@ wp_reset_postdata();
                         $html .= '</div>';
                         $html .= '<div class="title">';
                         $html .= '<span class="the-title">'. get_the_title().'</span>';
-                        $html .= '<span class="the-price">'. $product->get_regular_price() .'</span>';
+                        $html .= '<span class="the-price">£ '. $product->get_regular_price() .'</span>';
                         $html .= '<span class="the-time">'. $ticket_time .'</span>';
                         $html .= '</div>';
                         $html .= '</div>';

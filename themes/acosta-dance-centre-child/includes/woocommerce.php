8ff057a4834ab4b6375cca0a75c098dc80cddbef
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-06 14:18:37 +0800

    Update woocommerce.php

--- themes/acosta-dance-centre-child/includes/woocommerce.php
+++ themes/acosta-dance-centre-child/includes/woocommerce.php
@@ -476,7 +476,31 @@ function refresh_cart_count($fragments)
 function action_woocommerce_before_checkout_form()
 {
 ?>
-  sdsds
+  <div class="donation-box">
+    <div class="row">
+      <div class="col-lg-7">
+        <div class="column-holder details">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="image-box">
+                <img src="https://acostadancecentre.com/wp-content/uploads/2023/10/adc.png" alt="">
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="heading-box">
+                <h4>ACOSTA DANCE FOUNDATION</h4>
+              </div>
+              <div class="description-box">
+                <p>
+                  Did you know the Acosta Dance Centre is one of the main pillars of the Acosta Dance Foundation (ADF) Established in 2011 as a charity, the ADF strive to bring dance into every home. Please consider donating to the Acosta Dance Foundation today and make a meaningful difference in our cause.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 <?php
 }
 

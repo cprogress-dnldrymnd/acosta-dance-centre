c86572258e8b9b9e2c1d4443d3e520dc2910f883
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-30 23:32:22 +0800

    Update woocommerce.php

--- themes/acosta-dance-centre-child/includes/woocommerce.php
+++ themes/acosta-dance-centre-child/includes/woocommerce.php
@@ -26,7 +26,7 @@ function action_woocommerce_before_shop_loop_item()
   $ticket_day_week = date('D', $dateformat);
   $ticket_month = date('M', $dateformat);
   echo ' <div class="image-holder">';
-  ?>
+?>
   <div class="meta-date">
     <div class="m-d text-uppercase text-center">
       <span class="month d-block"><?= $ticket_day_week ?></span>
@@ -37,7 +37,7 @@ function action_woocommerce_before_shop_loop_item()
       <span class="time d-block text-uppercase month"><?= $ticket_time ?></span>
     </div>
   </div>
-  <?php
+<?php
 }
 
 add_action('woocommerce_before_shop_loop_item', 'action_woocommerce_before_shop_loop_item');
@@ -45,11 +45,11 @@ add_action('woocommerce_before_shop_loop_item', 'action_woocommerce_before_shop_
 function action_woocommerce_after_shop_loop_item_title()
 {
   echo '</div>';
-  ?>
+?>
   <h3>
     <?= get_the_title() ?>
   </h3>
-  <?php
+<?php
 }
 
 add_action('woocommerce_after_shop_loop_item_title', 'action_woocommerce_after_shop_loop_item_title');
@@ -133,7 +133,7 @@ function action_woocommerce_after_single_product()
   $ticket_time = single_get_date($product->get_id(), 'time');
 
   $_product_category = _product_category(get_the_ID());
-  ?>
+?>
 
   <div class="details-box position-relative">
     <h3 class="doro-heading">DETAILS</h3>
@@ -167,8 +167,7 @@ function action_woocommerce_after_single_product()
         <?php } ?>
         <?php do_action('woocommerce_product_additional_information', $product); ?>
       </table>
-    <?php }
-    else { ?>
+    <?php } else { ?>
       <?= get_the_excerpt() ?>
     <?php } ?>
 
@@ -178,13 +177,11 @@ function action_woocommerce_after_single_product()
         echo '<div class="button-box button-black">';
         echo ' <a href="#book-now">BOOK NOW</a>';
         echo '</div>';
-      }
-      else if ($_product_category == 'classes') {
+      } else if ($_product_category == 'classes') {
         echo '<div class="button-box button-black">';
         echo '<a href="?add-to-cart=' . $product->get_id() . '" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="' . $product->get_id() . '"  aria-describedby="" rel="nofollow">BOOK NOW</a>';
         echo '</div>';
-      }
-      else if ($_product_category == 'memberships') {
+      } else if ($_product_category == 'memberships') {
         echo ' <a href="#join-now">JOIN NOW</a>';
       }
       ?>
@@ -194,8 +191,7 @@ function action_woocommerce_after_single_product()
   <?php
   if ($_product_category != 'memberships') {
     echo do_shortcode('[membership_box]');
-  }
-  else { ?>
+  } else { ?>
     <div class="membership-side-buttons">
       <div class="button-box button-black mb-3">
         <a href="/category/tickets/classes/" class="w-100">FIND A DANCE CLASS</a>
@@ -205,7 +201,7 @@ function action_woocommerce_after_single_product()
       </div>
     </div>
   <?php } ?>
-  <?php
+<?php
   echo '</div>';
   echo '</div>';
   echo '</div>';
@@ -239,7 +235,7 @@ function action_single_product_after_image()
   $ticket_date = single_get_date(get_the_ID());
   $ticket_time = single_get_date(get_the_ID(), 'time');
   $_product_category = _product_category(get_the_ID());
-  ?>
+?>
   <div class="content-margin">
     <?php if ($text_after_image) { ?>
       <h2 class="doro-heading">
@@ -288,27 +284,29 @@ function action_single_product_after_image()
       </style>
       <?php
       global $product;
-      $variations = $product->get_available_variations();
-      $product_variation = array();
-      foreach ($variations as $variation) {
-        $product_variation[$variation['variation_id']] = $variation['attributes']['attribute_payment-plan'];
-      }
+      if ($product->get_type() == 'variable') {
+        $variations = $product->get_available_variations();
+        $product_variation = array();
+        foreach ($variations as $variation) {
+          $product_variation[$variation['variation_id']] = $variation['attributes']['attribute_payment-plan'];
+        }
       ?>
-      <div class="add-to-cart-box text-center md-padding-bottom" id="join-now">
-        <h3>A PLACE OF PASSION, ARTISTIC BRILLIANCE, AND BOUNDLESS CREATIVITY. JOIN THE ACOSTA DANCE CENTRE TODAY</h3>
-        <div class="button-group-box justify-content-center">
-          <?php foreach ($product_variation as $key => $variation) { ?>
-            <?php
-            $product = wc_get_product($key);
-            ?>
-            <div class="button-box <?= $variation == 'Monthly' ? 'button-black' : 'button-bordered' ?>">
-              <a href="?add-to-cart=<?= $key ?>">
-                PAY <?= $variation ?>
-              </a>
-            </div>
-          <?php } ?>
+        <div class="add-to-cart-box text-center md-padding-bottom" id="join-now">
+          <h3>A PLACE OF PASSION, ARTISTIC BRILLIANCE, AND BOUNDLESS CREATIVITY. JOIN THE ACOSTA DANCE CENTRE TODAY</h3>
+          <div class="button-group-box justify-content-center">
+            <?php foreach ($product_variation as $key => $variation) { ?>
+              <?php
+              $product = wc_get_product($key);
+              ?>
+              <div class="button-box <?= $variation == 'Monthly' ? 'button-black' : 'button-bordered' ?>">
+                <a href="?add-to-cart=<?= $key ?>">
+                  PAY <?= $variation ?>
+                </a>
+              </div>
+            <?php } ?>
+          </div>
         </div>
-      </div>
+      <?php } ?>
     <?php } ?>
 
     <?php if ($_product_category != 'memberships' && $_product_category != 'others') { ?>
@@ -318,7 +316,7 @@ function action_single_product_after_image()
     <?php } ?>
   </div>
 
-  <?php
+<?php
 }
 
 
@@ -344,14 +342,11 @@ function _product_category($id, $classes = 'classes', $workshops = 'workshops', 
   }
   if (in_array(79, $terms_val)) {
     return $classes;
-  }
-  else if (in_array(78, $terms_val)) {
+  } else if (in_array(78, $terms_val)) {
     return $workshops;
-  }
-  else if (in_array(88, $terms_val)) {
+  } else if (in_array(88, $terms_val)) {
     return $memberships;
-  }
-  else {
+  } else {
     return 'others';
   }
 }
@@ -365,11 +360,9 @@ function _product_category_id($id)
   }
   if (in_array(79, $terms_val)) {
     return 79;
-  }
-  else if (in_array(78, $terms_val)) {
+  } else if (in_array(78, $terms_val)) {
     return 78;
-  }
-  else if (in_array(88, $terms_val)) {
+  } else if (in_array(88, $terms_val)) {
     return 88;
   }
 }
@@ -398,8 +391,7 @@ function get_member_discount($memberships, $id)
     $discounted_price = '<span class="woocommerce-Price-amount amount"><bdi><span class="woocommerce-Price-currencySymbol">£</span>' . sprintf("%.2f", $product->get_price()) . '</bdi></span>';
     $discount_amount_val = ' <span>(' . $discount_amount . ')</span>';
     return $regular_price . ' ' . $discounted_price . $discount_amount_val;
-  }
-  else {
+  } else {
     return 'NONE';
   }
 }
@@ -471,12 +463,12 @@ add_filter('woocommerce_add_to_cart_fragments', 'refresh_cart_count', 50, 1);
 function refresh_cart_count($fragments)
 {
   ob_start();
-  ?>
+?>
   <span class="counter" id="cart-count"><?php
-  $cart_count = WC()->cart->get_cart_contents_count();
-  echo sprintf(_n('%d', '%d', $cart_count), $cart_count);
-  ?></span>
-  <?php
+                                        $cart_count = WC()->cart->get_cart_contents_count();
+                                        echo sprintf(_n('%d', '%d', $cart_count), $cart_count);
+                                        ?></span>
+<?php
   $fragments['#cart-count'] = ob_get_clean();
 
   return $fragments;
@@ -486,7 +478,7 @@ function refresh_cart_count($fragments)
 function action_woocommerce_before_checkout_form()
 {
 
-  ?>
+?>
   <div class="donation-box">
     <div class="row">
       <div class="col-lg-7">
@@ -518,16 +510,13 @@ function action_woocommerce_before_checkout_form()
           <div class="donation-price">
             <ul class="list-inline">
               <li>
-                <div value="10" class="donation d-flex justify-content-between align-items-center"><span
-                    class="amount">£10</span></div>
+                <div value="10" class="donation d-flex justify-content-between align-items-center"><span class="amount">£10</span></div>
               </li>
               <li>
-                <div value="5" class="donation d-flex justify-content-between align-items-center"><span
-                    class="amount">£5</span></div>
+                <div value="5" class="donation d-flex justify-content-between align-items-center"><span class="amount">£5</span></div>
               </li>
               <li>
-                <div value="0" class="donation d-flex justify-content-between align-items-center"><span
-                    class="amount">SPECIFY AN AMOUNT</span></div>
+                <div value="0" class="donation d-flex justify-content-between align-items-center"><span class="amount">SPECIFY AN AMOUNT</span></div>
                 <div class="input-holder"><input type="number" class="custom-amount" placeholder="£"></div>
               </li>
             </ul>
@@ -551,7 +540,7 @@ function action_woocommerce_before_checkout_form()
       </div>
     </div>
   </div>
-  <?php
+<?php
 }
 
 add_action('woocommerce_before_checkout_form', 'action_woocommerce_before_checkout_form');
